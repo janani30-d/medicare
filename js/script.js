6b3b8fac85c8b4fbca8d418e1cfdab7763ffb18b
janani senthilram <jaanusenthilram14@gmail.com>
2026-08-24 14:29:15 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -685,3 +685,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+/* =========================================================
+   SCROLL TO TOP
+========================================================= */
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+if (scrollTopBtn) {
+
+    window.addEventListener("scroll", function () {
+
+        if (window.scrollY > 300) {
+            scrollTopBtn.classList.add("show");
+        } else {
+            scrollTopBtn.classList.remove("show");
+        }
+
+    });
+
+    scrollTopBtn.addEventListener("click", function () {
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+    });
+
+}
